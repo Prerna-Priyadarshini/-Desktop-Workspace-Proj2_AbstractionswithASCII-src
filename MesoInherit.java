dc7b7178332f629c341cf02b9a30ff12ac5258ba
Prerna Priyadarshini <prerna.priyadarshini-101@ou.edu>
2019-10-09 16:11:57 -0500

    made an array called x and made 4 variables and got their total to get their average; in the if and else statement Math.ceil and floor were implement

--- MesoInherit.java
+++ MesoInherit.java
@@ -10,7 +10,26 @@ public class MesoInherit extends MesoAbstract {
 	@Override
 	int[] calAverage() {
 		// TODO Auto-generated method stub
-		return null;
+		int[] x = new int[3];
+		int a = (int) stationID.getStID().charAt(0);
+		int b = (int) stationID.getStID().charAt(1);
+		int c = (int) stationID.getStID().charAt(2);
+		int d = (int) stationID.getStID().charAt(3);
+		int total = a + b + c + d;
+		double sum = (double) total;
+		double average = sum / 4;
+		
+		x[0] = (int) Math.ceil(average);
+		x[1] = (int) Math.floor(average);
+		if (average - x[1] >= 0.5) {
+			x[2] = (int) Math.ceil(average);
+		}
+		else {
+			x[2] = (int) Math.floor(average);
+		}
+		
+		
+		return x;
 	}
 
 	@Override
