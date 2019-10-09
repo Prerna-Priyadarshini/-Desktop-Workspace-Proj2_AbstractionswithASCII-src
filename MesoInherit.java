e867912992c60da17d1d066b824293fff755771d
Prerna Priyadarshini <prerna.priyadarshini-101@ou.edu>
2019-10-09 16:05:28 -0500

    Extended the MesoInherit class to the MesoAbstract class and made a private variable stationID and it the constructor initialized it to a

--- MesoInherit.java
+++ MesoInherit.java
@@ -1,4 +1,22 @@
 
-public class MesoInherit {
+public class MesoInherit extends MesoAbstract {
+	
+	private MesoStation stationID;
+	
+	public MesoInherit (MesoStation a) {
+		stationID = a;
+	}
+
+	@Override
+	int[] calAverage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	char letterAvg() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
